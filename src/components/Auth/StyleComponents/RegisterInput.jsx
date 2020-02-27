53258fbc502e022e37d9240cfe4a0351744e6563
Arthur K <arthur.kleiber@epitech.eu>
2020-02-27 18:50:18 +0100

    Small fix

--- src/components/Auth/StyleComponents/RegisterInput.jsx
+++ src/components/Auth/StyleComponents/RegisterInput.jsx
@@ -10,7 +10,7 @@ import PropTypes from 'prop-types';
 const styles = StyleSheet.create({
   label: {
     fontWeight: '500',
-    flex: 1,
+    width: '100%',
     color: '#FFF',
   },
   labelContainer: {
